db0a2145e01e2fa525e118b45c4e12524a373ff5
Jerrold Thompson <jet@whidbey.com>
2015-04-04 12:09:12 -0700

    scaffold contact

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ContactPage::Application.config.secret_key_base = '9018019d6f7c6e7db0a36581ebd7a7d8eec52931f99c4d6cbe98049ee1910af39655d2554c5365b62b99e248c56610d4d3533aafe2700e63577021a6a4afe680'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ContactPage::Application.config.session_store :cookie_store, key: '_contact-page_session'
